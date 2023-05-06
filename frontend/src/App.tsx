0632433509a05ee9661ff1eeffc0d126d29047c3
foobar <uagnl25x@anonaddy.me>
2023-05-06 23:59:59 +0530

    added privagte msg

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,74 @@
-import { useEffect } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import './App.css'
 import { io } from 'socket.io-client'
 
-const socket = io('http://localhost:8080')
+const socket = io('http://localhost:8080', { autoConnect: true })
+
+interface User {
+    id: string
+    username: string
+    self?: boolean
+}
+
+// const onMessage = content => {
+//     if (this.selectedUser) {
+//         socket.emit('private message', {
+//             content,
+//             to: this.selectedUser.userID,
+//         })
+//         this.selectedUser.messages.push({
+//             content,
+//             fromSelf: true,
+//         })
+//     }
+// }
 
 function App() {
+    const randomCharUntrimmed = crypto.randomUUID().toString()
+    const randomChar = randomCharUntrimmed.slice(32)
+    const username = useRef('vuban_' + randomChar)
+    const users = useRef<User[]>([])
+    const [custValue, setCustValue] = useState('')
+
     useEffect(() => {
-        socket.on('connection', () => {
-            console.log('connection establishd on clientside')
+        socket.auth = { username: username.current }
+
+        socket.onAny((event, ...args) => {
+            console.log('onAny ran:', event, args)
+        })
+
+        socket.on('connect', () => {
+            console.log('connection establishd on client-side with id:', socket.id)
+        })
+
+        socket.on('users', (users: User[]) => {
+            users.forEach(user => {
+                user.self = user.id === socket.id
+            })
+        })
+
+        socket.on('user connected', (user: User) => {
+            users.current.push(user)
+        })
+
+        socket.on('private message', ({ content, from }) => {
+            console.log('message from user', from, ':::-->>', content)
         })
     }, [])
 
     return (
         <>
             <div>
-                <b>Socket.io</b>
+                <b>Socket.io test run</b>
+                <br />
+                <input onChange={(e)=>setCustValue(e.target.value)} value={custValue}/>
+                <input
+                    type='button'
+                    value='emit'
+                    onClick={() => {
+                        socket.emit('private message', { content: 'hellowold', to: socket.users })
+                    }}
+                />
             </div>
         </>
     )
--- server/src/index.ts
+++ server/src/index.ts
@@ -4,8 +4,48 @@ import { Server } from 'socket.io'
 const httpServer = createServer()
 const io = new Server(httpServer, { cors: { origin: 'http://localhost:5173' } })
 
+declare module 'socket.io' {
+    interface Socket {
+        username?: string
+    }
+}
+
+interface User {
+    userID: string
+    username: string
+}
+
+io.use((socket, next) => {
+    const username = socket.handshake.auth.username
+    if (username === undefined) return next(new Error('invalid username'))
+    socket.username = username
+    next()
+})
+
 io.on('connection', socket => {
-    console.log('socket ready on server.', socket.id)
+    const users: User[] = []
+
+    for (const [id, socket] of io.of('/').sockets) {
+        users.push({
+            userID: id,
+            username: socket.username ?? 'undef00',
+        })
+    }
+
+    console.log('new user', socket.username, 'connected.')
+    console.log('users are;', users)
+
+    socket.broadcast.emit('user connected', {
+        userID: socket.id,
+        username: socket.username,
+    })
+
+    socket.on('private message', ({ content, to }: { [key: string]: string }) => {
+        socket.to(to).emit('private message', {
+            content,
+            from: socket.id, // This should not work because from: socket.id would be wrong... i think...
+        })
+    })
 })
 
 httpServer.listen(8080, () => {
